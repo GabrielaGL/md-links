a1ff2bf4cd7ab3902426d633d30700c3b3ab86a8
Gaabs <ggaabrielagomez@gmail.com>
2023-05-04 20:10:56 -0600

    Funcion para identificar si una ruta es relativa o absoluta

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ import path from 'path';
 
 const filePath = 'README.md'
 
-const readFile = fs.readFile(filePath, 'utf8', (err, data) => {
+function comprobarRutaAbsoluta(filePath) {
+  const itsAbsolute = path.isAbsolute(filePath);
+
+  if (itsAbsolute) {
+    console.log(`La ruta '${filePath}' es absoluta.`);
+  } else {
+    console.log(`La ruta '${filePath}' es relativa.`);
+    const convertAbsolute = path.resolve(filePath);
+    console.log(`La ruta absoluta es '${convertAbsolute}'.`);
+    comprobarRutaAbsoluta(convertAbsolute);
+  }
+}
+
+comprobarRutaAbsoluta(filePath)
+
+/* const readFile = fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
   console.log(data);
-});
+}); */
 
 
 
