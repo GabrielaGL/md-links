282dc7da3dd22774a998a70c230abfc807b45553
Gaabs <ggaabrielagomez@gmail.com>
2023-05-08 11:45:31 -0600

    Test Unitarios 60% coverage

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,45 @@
-const mdLinks = require('../');
+import { testRelativeAbsolute, getFiles, getMDExt, checkLink } from "../src/mdlinks.js";
+import chalk from 'chalk';
 
 
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe('testRelativeAbsolute', () => {
+  test('returns true for an absolute path', () => {
+    const result = testRelativeAbsolute('/path/to/file');
+    expect(result).toBe(true);
   });
 
+  test('returns an error message for an invalid path', () => {
+    console.error = jest.fn();
+    const result = testRelativeAbsolute('');
+    expect(console.error).toHaveBeenCalledWith(chalk.bold.red('La ruta no es válida. Intenta con una ruta válida'));
+  });
+});
+
+
+
+describe('getMDExt', () => {
+  test('should return an array with one file path if the file has .md extension', () => {
+    const filePath = '/path/to/file.md';
+    const expectedOutput = ['/path/to/file.md'];
+    expect(getMDExt(filePath)).toEqual(expectedOutput);
+  });
+
+  test('should return an empty array if the file does not have .md extension', () => {
+    const filePath = '/path/to/file.txt';
+    const expectedOutput = false;
+    expect(getMDExt(filePath)).toBe(expectedOutput);
+  });
+});
+
+
+describe('checkLink', () => {
+  it('should return true for a valid link', async () => {
+    const result = await checkLink('https://www.google.com');
+    expect(result).toBe(true);
+  });
+
+  it('should return false for an invalid link', async () => {
+    const result = await checkLink('https://www.thislinkdoesnotexist.com');
+    expect(result).toBe(false);
+  });
 });
